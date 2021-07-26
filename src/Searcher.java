58e9bf9b2955d10f8413846ac683dfbd5f95eb6b
Arash <arashyadegari0402@gmail.com>
2021-07-26 00:08:43 -0700

    reduce code complexity

--- src/Searcher.java
+++ src/Searcher.java
@@ -7,11 +7,9 @@ import java.util.Scanner;
 
 public class Searcher {
     private static final Searcher INSTANCE = new Searcher();
-    private final Comparator<String> queryComparator;
     private final DataContainer dataContainer;
 
     {
-        queryComparator = initializeComparator();
         dataContainer = DataContainer.getInstance();
     }
 
@@ -22,41 +20,40 @@ public class Searcher {
         return INSTANCE;
     }
 
-    private Comparator<String> initializeComparator() {
-        return this::compareWords;
-    }
-
-    private int compareWords(String word1, String word2) {
-        if (word1.startsWith("+"))
-            return -1;
-        if (word2.startsWith("+"))
-            return 1;
-        if (word1.startsWith("-"))
-            return -1;
-        if (word2.startsWith("-"))
-            return 1;
-        return 0;
-    }
-
     public void run() {
         String input;
         while (true) {
             input = getInput();
             input = processInput(input);
+
             String[] words = input.split("[ ]+");
-            ArrayList<Integer> results = dataContainer.getAddress(words[words.length - 1]);
+            ArrayList<Integer> results = dataContainer.getAddress(words[0]);
 
-            ArrayList<String> wordsInOrder = sortWordsQuery(words);
+            ArrayList<Word> separateWords = getWords(words);
 
-            TypeChecker typeChecker = new TypeChecker(wordsInOrder);
-            ArrayList<Word> separateWords = typeChecker.getWords();
-
-            for (Word separateWord : separateWords) {
-                separateWord.action(dataContainer.getAddress(separateWord.getWordInString()), results);
-            }
+            sortWords(separateWords);
+            mergeSearchResults(results, separateWords);
 
             System.out.println(results);
         }
+    }
+
+    private void mergeSearchResults(ArrayList<Integer> results, ArrayList<Word> separateWords) {
+        for (Word separateWord : separateWords) {
+            separateWord.action(dataContainer.getAddress(separateWord.getWordInString()), results);
+        }
+    }
+
+    private ArrayList<Word> getWords(String[] words) {
+        ArrayList<String> wordsAsArrayList = new ArrayList<>(Arrays.asList(words));
+
+        TypeChecker typeChecker = new TypeChecker(wordsAsArrayList);
+        return typeChecker.getWords();
+    }
+
+    private void sortWords(ArrayList<Word> separateWords) {
+        Comparator<Word> comparator = Comparator.comparing(Word::getPriority);
+        separateWords.sort(comparator);
     }
 
     private String getInput() {
@@ -70,10 +67,4 @@ public class Searcher {
         return input;
     }
 
-    private ArrayList<String> sortWordsQuery(String[] words) {
-        // Making "words" an arraylist to sort it
-        ArrayList<String> wordAsAnArrayList = new ArrayList<>(Arrays.asList(words));
-        wordAsAnArrayList.sort(this.queryComparator);
-        return wordAsAnArrayList;
-    }
 }
