11bf305ab6c16e33dc892b247b0074878aeb95d5
Arash <arashyadegari0402@gmail.com>
2021-07-27 13:21:15 -0700

    refactor

--- src/modifiers/TypeChecker.java
+++ src/modifiers/TypeChecker.java
@@ -18,13 +18,18 @@ public class TypeChecker {
     public ArrayList<Word> separateWords(ArrayList<String> words) {
         for (String word : words) {
             Word wordInObject;
-
-            if (word.charAt(0) == '+') wordInObject = new PositiveWord(word);
-            else if (word.charAt(0) == '-') wordInObject = new NegativeWord(word);
-            else wordInObject = new NeutralWord(word);
+            wordInObject = checkWordCondition(word);
             wordsInObject.add(wordInObject);
         }
         return sortResult(wordsInObject);
+    }
+
+    private Word checkWordCondition(String word) {
+        Word wordInObject;
+        if (word.charAt(0) == '+') wordInObject = new PositiveWord(word);
+        else if (word.charAt(0) == '-') wordInObject = new NegativeWord(word);
+        else wordInObject = new NeutralWord(word);
+        return wordInObject;
     }
 
     private ArrayList<Word> sortResult(ArrayList<Word> wordsInObject) {
