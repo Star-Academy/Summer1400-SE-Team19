92e64811aac55914a29d1ee7eb51fe9915d25b29
aradmaleki02 <arad.m.t02@gmail.com>
2021-07-31 18:25:01 +0430

    alomst done with tests!

---
+++ src/test/java/ParameterHolderInitializerTest.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class ParameterHolderInitializerTest {
+    private ParameterHolderInitializer parameterHolderInitializer;
+
+    @Test
+    public void initializationTest() {
+        parameterHolderInitializer = new ParameterHolderInitializer();
+    }
+}
