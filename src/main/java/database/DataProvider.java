bd9f0f6a5d287290724eef7fe8d2bf97557e6a49
Arash <arashyadegari0402@gmail.com>
2021-07-31 12:34:11 -0700

    fix a bug

--- src/main/java/database/DataProvider.java
+++ src/main/java/database/DataProvider.java
@@ -5,6 +5,7 @@ import parameterholders.abstraction.DataProviderParametersInterface;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Objects;
+import java.util.Scanner;
 
 public class DataProvider {
     private final DataContainer dataContainer;
@@ -25,6 +26,7 @@ public class DataProvider {
         for (File file : Objects.requireNonNull(fileDirectory.listFiles())) {
             try {
                 reader.setFile(file);
+                reader.setScanner(new Scanner(file));
                 reader.read();
             } catch (FileNotFoundException ignored) {
             }
--- src/test/java/database/FileReaderTest.java
+++ src/test/java/database/FileReaderTest.java
@@ -6,15 +6,18 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.io.ByteArrayInputStream;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
 import java.util.Scanner;
 
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
+
 public class FileReaderTest {
-    @Mock
-    Scanner scanner;
+
     @Mock
     File file;
     @Mock
@@ -29,5 +32,18 @@ public class FileReaderTest {
     public void avoidNonAlphabeticalSignsTest() {
         FileReader fileReader = new FileReader(dataContainer);
         fileReader.setFile(file);
+        try {
+            fileReader.setScanner(new Scanner(file));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        String input = "one two three";
+        InputStream in = new ByteArrayInputStream(input.getBytes());
+        System.setIn(in);
+        try {
+            fileReader.read();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 }
