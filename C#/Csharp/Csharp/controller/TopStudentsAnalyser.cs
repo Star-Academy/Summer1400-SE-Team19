84ba105fc3f47917c0ca4e0d705fc6ed85371681
aradmaleki02 <arad.m.t02@gmail.com>
2021-08-03 15:17:14 +0430

    finds top students

--- C#/Csharp/Csharp/controller/TopStudentsAnalyser.cs
+++ C#/Csharp/Csharp/controller/TopStudentsAnalyser.cs
@@ -1,5 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
+using System.Linq;
 using Csharp.model;
 
 namespace Csharp.controller
@@ -7,10 +7,37 @@ namespace Csharp.controller
     public class TopStudentsAnalyser : IDataAnalyser<Student>
     {
         public Dictionary<Student, List<Grade>> StudentGradesDictionary { get; set; }
+        public Dictionary<Student, double> StudentGpaDictionary;
         
         public List<Student> Analyse()
         {
-            throw new NotImplementedException(); 
+            StudentsAndGradesProvider studentsAndGradesProvider = new StudentsAndGradesProvider();
+            StudentGradesDictionary = studentsAndGradesProvider.Provide(Student.AllStudents, Grade.AllGrades);
+            CalculateGpaForEachStudent();
+            return SelectTopStudents(3);
+        }
+
+        private List<Student> SelectTopStudents(int numberOfStudents)
+        {
+            List<Student> topStudents = new List<Student>();
+            for (int i = 0; i < numberOfStudents; i++)
+            {
+                Student topStudent = StudentGpaDictionary.OrderByDescending(x => x.Value).Skip(i).First().Key;
+                topStudents.Add(topStudent);
+            }
+            return topStudents;
+        }
+
+        private void CalculateGpaForEachStudent()
+        {
+            StudentGpaDictionary = new Dictionary<Student, double>();
+            foreach (var iterator in StudentGradesDictionary)
+            {
+                var student = iterator.Key;
+                var grades = iterator.Value;
+                double gpa = grades.Average(g => g.Score); 
+                StudentGpaDictionary.Add(student, gpa);
+            }
         }
     }
 }
