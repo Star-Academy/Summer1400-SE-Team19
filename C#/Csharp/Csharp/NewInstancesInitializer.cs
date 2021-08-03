3d5cab7c93feac43c1b1fdd62f77e502505f7cf8
aradmaleki02 <arad.m.t02@gmail.com>
2021-08-03 18:22:12 +0430

    added new classes

---
+++ C#/Csharp/Csharp/NewInstancesInitializer.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using Csharp.controller;
+using Csharp.model;
+using Csharp.view;
+
+namespace Csharp
+{
+    public class NewInstancesInitializer
+    {
+        public ParameterHolder ParameterHolder { get; }
+
+        public NewInstancesInitializer()
+        {
+            ParameterHolder = new ParameterHolder();
+        }
+
+        public void Initialize()
+        {
+            ParameterHolder.GradesStreamReader = new StreamReader(Path.GetFullPath("resources/Grades.json"));
+            ParameterHolder.StudentsStreamReader = new StreamReader(Path.GetFullPath("resources/Students.json"));
+            ParameterHolder.StudentGpaDictionary = new Dictionary<Student, double>();
+            ParameterHolder.StudentsAndGradesProvider = new StudentsAndGradesProvider();
+            ParameterHolder.DisplayStudents = new DisplayStudents();
+            ParameterHolder.TopStudents = new Dictionary<Student, double>();
+            ParameterHolder.TopStudentsAnalyzer = new TopStudentsAnalyser(ParameterHolder);
+            ParameterHolder.FileReader = new JsonFileReader(ParameterHolder);
+        }
+    }
+}
---
+++ C#/Csharp/Csharp/model/ParameterHolder.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using Csharp.controller;
+using Csharp.view;
+
+namespace Csharp.model
+{
+    public class ParameterHolder
+    {
+        public StreamReader StudentsStreamReader { get; set; }
+        public StreamReader GradesStreamReader { get; set; }
+        public Dictionary<Student, double> StudentGpaDictionary { get; set; }
+        public IDataProvider<Student, Grade> StudentsAndGradesProvider { get; set; }
+        public IDisplay<Student, double> DisplayStudents { get; set; }
+        public IDataAnalyser<Student, double> TopStudentsAnalyzer { get; set; }
+        public IReader FileReader { get; set; }
+        public Dictionary<Student, double> TopStudents { get; set; }
+    }
+}
