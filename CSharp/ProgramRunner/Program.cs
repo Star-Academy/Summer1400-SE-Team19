eea3db53116b8b10392dae28b7ff06de221e0694
aradmaleki02 <arad.m.t02@gmail.com>
2021-08-08 13:54:35 +0430

    added data provider

--- CSharp/ProgramRunner/Program.cs
+++ CSharp/ProgramRunner/Program.cs
@@ -1,20 +1,24 @@
 ﻿using System;
+using System.Collections.Generic;
 using InvertedIndexSearcher;
+using InvertedIndexSearcher.database;
 using InvertedIndexSearcher.modifiers;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
 namespace ProgramRunner
 {
-    class Program
+    static class Program
     {
         private static IServiceProvider _serviceProvider;
 
-        static void Main(string[] args)
+        static void Main()
         {
             CreateHost();
+            var dataProvider = (DataProvider) _serviceProvider.GetService(typeof(DataProvider));
+            dataProvider?.Initialize();
             var userInterface = (UserInterface) _serviceProvider.GetService(typeof(UserInterface));
-            userInterface.Run();
+            userInterface?.Run();
         }
 
         private static void CreateHost()
@@ -28,8 +32,9 @@ namespace ProgramRunner
                 services.AddSingleton<ITypeChecker, TypeChecker>();
                 services.AddSingleton<IDataCollector, DataCollector>();
                 services.AddSingleton<ISearcher, Searcher>();
-                // services.AddSingleTon<interface, class>();
-                // order of implementation is important!!!
+                services.AddSingleton<IFileReader, FileReader>();
+                services.AddSingleton<IDataContainer<string, HashSet<int>>, DataContainer>();
+                services.AddSingleton<DataProvider>();
             }).Build();
             _serviceProvider = host.Services;
         }
