3c70770ecdf93ae06fc5ab311b1bab34ec76b92d
Arash Yadegari <79264867+Arash1381-y@users.noreply.github.com>
2021-08-08 06:47:40 -0700

    Update SQLQuery_1.sql

--- SQL/SQLQuery_1.sql
+++ SQL/SQLQuery_1.sql
@@ -1,3 +1,4 @@
+SELECT * INTO newTable FROM Students LEFT JOIN Grades ON Students.StudentNumber = Grades.StudentNumber; 
 SELECT StudentNumber  , AVG(Score) AS GPA INTO studentAndAvarge  FROM newTable GROUP BY StudentNumber; 
 SELECT TOP(3)  studentAndAvarge.StudentNumber , FirstName , LastName , GPA INTO phase7  FROM  studentAndAvarge
  LEFT JOIN Students ON studentAndAvarge.StudentNumber = Students.StudentNumber ORDER BY GPA DESC;
