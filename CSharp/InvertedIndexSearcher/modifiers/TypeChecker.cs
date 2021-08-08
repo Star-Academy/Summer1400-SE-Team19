f7c7f3098c8246f0643920766d5bac7c9f5a5221
Arash <arashyadegari0402@gmail.com>
2021-08-08 10:25:28 -0700

    refactor container

---
+++ CSharp/InvertedIndexSearcher/modifiers/TypeChecker.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using InvertedIndexSearcher.words;
+
+namespace InvertedIndexSearcher.modifiers
+{
+    public class TypeChecker
+    {
+        public List<IWord> PositiveWords { get; }
+        public List<IWord> NeutralWords { get; }
+        public List<IWord> negativeWords { get; }
+
+        public TypeChecker()
+        {
+            PositiveWords = new List<IWord>();
+            NeutralWords = new List<IWord>();
+            negativeWords = new List<IWord>();
+        }
+
+        public void CheckWordsType(IEnumerable<string> words)
+        {
+            foreach (var word in words)
+            {
+                if (word.StartsWith("+")) PositiveWords.Add(new PositiveWord(word));
+                else if (word.StartsWith("-")) negativeWords.Add(new NegativeWord(word));
+                else NeutralWords.Add(new NeutralWord(word));
+            }
+        }
+    }
+}
