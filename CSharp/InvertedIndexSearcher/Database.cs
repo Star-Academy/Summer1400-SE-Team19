bdf01cbacbe05ebebb77153aeb0b8c7d392ba7dc
Arash <arashyadegari0402@gmail.com>
2021-08-09 21:39:23 -0700

    add Database class

---
+++ CSharp/InvertedIndexSearcher/Database.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace InvertedIndexSearcher
+{
+    public class Database : DbContext
+    {
+        public DbSet<WordAndAddressWrapper> WordAndAddressWrappers { set; get; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=.\SQLEXPRESS;Database=WordsDB;Trusted_Connection=True;");
+        }
+    }
+}
