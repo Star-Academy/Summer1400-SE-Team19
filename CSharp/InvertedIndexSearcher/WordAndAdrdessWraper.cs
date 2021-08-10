a48710f40456545ece78f2cfa500d725522d42e2
Arash <arashyadegari0402@gmail.com>
2021-08-09 21:38:58 -0700

    add DB context class

---
+++ CSharp/InvertedIndexSearcher/WordAndAdrdessWraper.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace InvertedIndexSearcher
+{
+    public class WordAndAddressWrapper
+    {
+        
+        [Key]
+        public string Word { get; set; }
+        public int Address { get; set; }
+    }
+}
