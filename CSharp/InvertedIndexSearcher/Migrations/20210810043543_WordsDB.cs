45300371b615f8056195555031ebba24a24eaa47
Arash <arashyadegari0402@gmail.com>
2021-08-09 21:46:26 -0700

    migration and word

---
+++ CSharp/InvertedIndexSearcher/Migrations/20210810043543_WordsDB.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace InvertedIndexSearcher.Migrations
+{
+    public partial class WordsDB : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "WordAndAddressWrappers",
+                columns: table => new
+                {
+                    Word = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Address = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_WordAndAddressWrappers", x => x.Word);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "WordAndAddressWrappers");
+        }
+    }
+}
