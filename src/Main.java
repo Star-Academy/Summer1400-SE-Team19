409bc5f7d5d993b24996968d55cc10cb1759eb18
Arash <arashyadegari0402@gmail.com>
2021-08-14 12:27:39 -0700

    commit alaki

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         try {
+        
             HashmapInitializer hashmapInitializer = new HashmapInitializer();
             hashmapInitializer.initialize();
             new Searcher(hashmapInitializer).run();
