360b520b121bb5994f000d9db68bd01aa78899e8
Arash <arashyadegari0402@gmail.com>
2021-08-14 12:41:49 -0700

    fix conflicts

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,14 @@
+import database.DataProvider;
+import parameterholders.abstraction.ParameterHolderInterface;
+
+import java.io.FileNotFoundException;
+
 public class Main {
-    public static void main(String[] args) {
-        try { 
-            HashmapInitializer hashmapInitializer = new HashmapInitializer();
-            hashmapInitializer.initialize();
-            new Searcher(hashmapInitializer).run();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+    public static void main(String[] args) throws FileNotFoundException {
+        ParameterHolderInterface parameterHolder = new ParameterHolderInitializer().getParameterHolder();
+        DataProvider dataProvider = new DataProvider(parameterHolder.getDataProviderParameters());
+        dataProvider.initialize();
+        UserInterface userInterface = new UserInterface(parameterHolder.getUserInterfaceParameters());
+        userInterface.run();
     }
 }
