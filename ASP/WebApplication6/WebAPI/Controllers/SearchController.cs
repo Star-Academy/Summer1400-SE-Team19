e53a138f74403bfdd6195bdf9b553f59de5d8fbf
aradmaleki02 <arad.m.t02@gmail.com>
2021-08-17 13:54:10 +0430

    First initialization

---
+++ ASP/WebApplication6/WebAPI/Controllers/SearchController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication6.Controllers
+{
+    
+    [ApiController]
+    [Route("[controller]")]
+    public class SearchController : ControllerBase
+    {
+
+        [HttpGet("{input}")]
+        [Route("text")]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(string))]
+        public IActionResult SearchText(string input)
+        {
+            return null;
+        }
+    }
+}
--- ASP/WebApplication6/WebAPI/Startup.cs
+++ ASP/WebApplication6/WebAPI/Startup.cs
@@ -11,6 +11,11 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using InvertedIndexSearcher;
+using InvertedIndexSearcher.database;
+using InvertedIndexSearcher.database.dataproviders;
+using InvertedIndexSearcher.modifiers;
+using InvertedIndexSearcher.modifiers.filterers;
 
 namespace WebApplication6
 {
@@ -31,6 +36,15 @@ namespace WebApplication6
             {
                 c.SwaggerDoc("v1", new OpenApiInfo {Title = "WebAPI", Version = "v1"});
             });
+            services.AddSingleton<IPositivesWordFilterer, PositivesWordFilterer>();
+            services.AddSingleton<INeutralsWordFilterer, NeutralsWordFilterer>();
+            services.AddSingleton<INegativesWordFilterer, NegativesWordFilterer>();
+            services.AddSingleton<ITypeChecker, TypeChecker>();
+            services.AddSingleton<IDataCollector, DataCollector>();
+            services.AddSingleton<ISearcher, Searcher>();
+            services.AddSingleton<IFileReader, FileReader>();
+            services.AddSingleton<Database>();
+            services.AddSingleton<IDataProvider, DefaultDataProvider>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
