4c6d7084055720e675504d59d5a287bbf9d1921a
Arash <arashyadegari0402@gmail.com>
2021-08-19 20:46:19 -0700

    database will be defined from out of the library

--- CSharp/InvertedIndexSearcher/modifiers/DataCollector.cs
+++ CSharp/InvertedIndexSearcher/modifiers/DataCollector.cs
@@ -1,18 +1,24 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
+using InvertedIndexSearcher.database;
 using InvertedIndexSearcher.words;
 
 namespace InvertedIndexSearcher.modifiers
 {
     public class DataCollector : IDataCollector
     {
-        public IEnumerable<HashSet<int>> Collect(IEnumerable<IWord> words)
+        private readonly Database _database;
+
+        public DataCollector(Database database)
         {
-            var listOfSearchedResultOfWords = new List<HashSet<int>>();
+            _database = database;
+        }
+        public IEnumerable<HashSet<string>> Collect(IEnumerable<IWord> words)
+        {
+            var listOfSearchedResultOfWords = new List<HashSet<string>>();
             foreach (var word in words)
             {
-                using var context = new Database();
-                var resultOfSearch = context.WordAndAddressWrapper
+                var resultOfSearch = _database.WordAndAddressWrapper
                     .Where(x => x.Word == word.WordAsString)
                     .Select(x => x.Address).ToHashSet();
                 listOfSearchedResultOfWords.Add(resultOfSearch);
