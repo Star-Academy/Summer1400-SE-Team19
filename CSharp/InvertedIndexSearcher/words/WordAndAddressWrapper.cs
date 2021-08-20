e7de3fe705863c714770e977e1e336c7890f7d3a
Arash <arashyadegari0402@gmail.com>
2021-08-19 20:49:05 -0700

    replace set methods with init for model

--- CSharp/InvertedIndexSearcher/words/WordAndAddressWrapper.cs
+++ CSharp/InvertedIndexSearcher/words/WordAndAddressWrapper.cs
@@ -1,15 +1,15 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
-namespace InvertedIndexSearcher
+namespace InvertedIndexSearcher.words
 {
     public class WordAndAddressWrapper
     {
-        
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
-        public string Word { get; set; }
-        public int Address { get; set; }
+
+        public string Word { get; init; }
+        public string Address { get; init; }
     }
 }
